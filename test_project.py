ca00d7bc1e87ea9b44c7bb005712037cb5bf0a21
Marco Lorusso <marco.lorusso96@gmail.com>
2020-04-03 16:51:46 +0200

    Test for training and immediately model retrieving from disk added for xgb (for NN the test was implied in the test for the setting of parameters

--- test_project.py
+++ test_project.py
@@ -4,6 +4,7 @@ import project
 from project import seed
 import pandas as pd
 import numpy as np
+import os
 from hypothesis import given
 import hypothesis.strategies as st
 import argparse
@@ -48,6 +49,7 @@ def test_training_failure(dat,n,ne,b,a):
 def test_set_param_NN():
     a = project.training_model("https://www.dropbox.com/s/v4sys56bqhmdfbd/fake.csv?dl=1", par = [5,5,0.2])
     b = project.model_upload("KerasNN_Model.joblib")
+    os.remove("KerasNN_Model.joblib")
     assert [b.get_params()['epochs'],b.get_params()['batch_size']] == [5,5]
 
 # def test_set_param_xgb():
@@ -72,6 +74,7 @@ def test_prediction_xgb_zeros():
     a = project.xgtrain("https://www.dropbox.com/s/v4sys56bqhmdfbd/fake.csv?dl=1",xgparams)
     c = project.prediction("https://www.dropbox.com/s/v4sys56bqhmdfbd/fake.csv?dl=1", "XGBoost_Model.joblib")
     b = np.zeros_like(c)
+    os.remove("XGBoost_Model.joblib")
     assert np.equal(c,b).all()
 
 def test_prediction_nn_zeros():
@@ -81,6 +84,7 @@ def test_prediction_nn_zeros():
     print(b)
     print(c)
     print(project.preprocessing("https://www.dropbox.com/s/v4sys56bqhmdfbd/fake.csv?dl=1"))
+    os.remove("KerasNN_Model.joblib")
     assert np.equal(c,b).all()
 
 def test_consistency_inference_xgb():
@@ -98,7 +102,22 @@ def test_consistency_inference_NN():
     assert np.equal(b,c).all()
     
     
-    
-    
+def test_training_loading_xgb():
+    xgparams = {'max_depth':5,
+                            'eta':0.3,
+                            'subsample':0.82,
+                            'colsample_bytree': 0.68,
+                            'eval_metric': ['merror','mlogloss'],
+                            'silent':0,
+                            'objective':'multi:softmax',
+                            'num_class':len(project.encoder.classes_),
+                            'seed':seed,
+                            'num_parallel_tree': 5
+                            #'tree_method': 'gpu_hist'
+                            }
+    a = project.xgtrain("https://www.dropbox.com/s/v4sys56bqhmdfbd/fake.csv?dl=1",xgparams)
+    b = project.model_upload("XGBoost_Model.joblib")
+    os.remove("XGBoost_Model.joblib")
+    return 0
     
     
